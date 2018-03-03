6c21a5de7c799763e4ee4524659d466a3eaa5fc4
hyginn <boris.steipe@utoronto.ca>
2018-03-03 17:48:35 -0500

    Use symbols from HGNC as authoritative gene list

--- prepareGenomeData.R
+++ prepareGenomeData.R
@@ -1,12 +1,12 @@
 # prepareGenomeData.R
 #
-# Purpose:  Read source data from provider databases, map to HUGO symbols
+# Purpose:  Read source data from provider databases, map to HGNC symbols
 #           and store as tab-delimied text.
 #
 #           Currently: linear data from HGNC only
 #
-# Version:  0.2
-# Date:     2018 02 20
+# Version:  0.3.1
+# Date:     2018 03 03
 # Author:   Boris Steipe <boris.steipe@utoronto.ca>
 #
 # Dependencies:
@@ -15,23 +15,29 @@
 # License: GPL-3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 #
 # Version history:
-#   0.2  add STRING data
-#   0.1  First draft: Gene data from BioMart
+#   0.3.1   Add gene type to basic data. Use HGNC symbols as authoritative
+#           source for gene annotations
+#   0.3     Remove BioMart GO data from basic gene data, prepare separate
+#             GO data file and populate Gene data with GOslim from BP
+#             ontology only.
+#   0.2     add STRING data
+#   0.1     First draft: Gene data from BioMart
 #
 # ToDo:
-#    - add different types of data
+#    - add GWAS and Protein data
 #
 # ==============================================================================
 
 
 #TOC> ==========================================================================
 #TOC> 
-#TOC>   Section  Title                              Line
-#TOC> --------------------------------------------------
-#TOC>   1        PARAMETERS                           39
-#TOC>   2        PACKAGES                             49
-#TOC>   3        BASIC GENE DATA (ENSEMBL)            64
-#TOC>   4        STRING DATA                         122
+#TOC>   Section  Title                                     Line
+#TOC> ---------------------------------------------------------
+#TOC>   1        PARAMETERS                                  45
+#TOC>   2        PACKAGES                                    57
+#TOC>   3        HGNC SYMBOLS AND CROSSREFERENCES            72
+#TOC>   4        BIOMART GENE ANNOTATIONS                   127
+#TOC>   5        STRING DATA                                171
 #TOC> 
 #TOC> ==========================================================================
 
@@ -42,8 +48,10 @@
 # are described below, but some of them are quite large so we are not
 # loading them into the repo.
 #
+HGNCDIR    <- "./"
 BIOMARTDIR <- "./"
 STRINGDIR  <- "./"
+GODIR      <- "./"
 
 
 # =    2  PACKAGES  ============================================================
@@ -61,7 +69,62 @@ if (!require(stringr, quietly=TRUE)) {
 
 
 
-# =    3  BASIC GENE DATA (ENSEMBL)  ===========================================
+# =    3  HGNC SYMBOLS AND CROSSREFERENCES  ====================================
+
+# The HGNC (Human Gene Nomeclature Committee) is the authoritative source for
+# recognized genes. Source data is downloaded from the custom download page
+# at https://www.genenames.org/cgi-bin/download
+#
+# - HGNC Approved Symbol
+# - Approved Name
+# - Locus Type
+# - Chromosome
+# - Ensembl Gene ID
+# - Entrez Gene ID (external)
+# - OMIM ID (external)
+# - RefSeq ID (external)
+# - UniProt ID (external)
+# - Ensembl ID (external)
+# - UCSC ID (external)
+
+# Download only "Approved" status, and Chr 20
+
+# read source data
+tmp <- read_tsv(paste0(HGNCDIR, "HGNC_data.tsv"))   # 1,041 rows
+
+# check which Locus types appear in this data
+unique(tmp$`Locus Type`)
+
+# subset rows with gene types of interest
+myLocusTypes <- c("gene with protein product",
+                  "RNA, transfer")
+
+tmp <- tmp[tmp$`Locus Type` %in% myLocusTypes, ]    # 529 rows
+
+# subset columns of interest
+Chr20GeneData <- data.frame(sym = tmp$`Approved Symbol`,
+                            name = tmp$`Approved Name`,
+                            type = tmp$`Locus Type`,
+                            EnsemblID = tmp$`Ensembl Gene ID`,
+                            EntrezGeneID = tmp$`Entrez Gene ID(supplied by NCBI)`,
+                            OMIMID = tmp$`OMIM ID(supplied by OMIM)`,
+                            RefSeqID = tmp$`RefSeq(supplied by NCBI)`,
+                            UniProtID = tmp$`UniProt ID(supplied by UniProt)`,
+                            UCSCID = tmp$`UCSC ID(supplied by UCSC)`,
+                            stringsAsFactors = FALSE)
+
+rownames(Chr20GeneData) <- Chr20GeneData$sym
+
+Chr20GeneData$type <- gsub("gene with protein product",
+                           "protein",
+                           Chr20GeneData$type)
+Chr20GeneData$type <- gsub("RNA, transfer",
+                           "tRNA",
+                           Chr20GeneData$type)
+
+
+
+# =    4  BIOMART GENE ANNOTATIONS  ============================================
 
 
 # Read file obtained via custom download from ensembl biomart
@@ -74,52 +137,38 @@ if (!require(stringr, quietly=TRUE)) {
 # Dataset: Human genes (GRCh38.p10)
 #
 # Filters: REGION: Chromosome/scaffold: 20
-#          GENE: With HGNC Symbol ID(s): Only
 # Attrib.: Gene stable ID
 #          Gene start (bp)
 #          Gene end (bp)
 #          Strand
-#          GOSlim GOA Accession(s)
-#          GOSlim GOA Description
 #          HGNC symbol
 
-tmp <- read_tsv(paste0(BIOMARTDIR, "mart_export.txt"))
-mySymbols <- sort(unique(tmp$`HGNC symbol`))
+# read source data
+tmp <- read_tsv(paste0(BIOMARTDIR, "mart_export.txt"))   # 5,379 rows
 
-myGOslimsTable <- table(tmp$`GOSlim GOA Accession(s)`)
+# use only rows that match Ensembl IDs in Chr20GeneData
+tmp <- tmp[(tmp$`Gene stable ID` %in% Chr20GeneData$EnsemblID), ]  # 521 rows
 
-N <- length(mySymbols)
-myGeneData <- data.frame(sym = mySymbols,
-                         chr = rep(20, N),
-                         start = numeric(N),
-                         end = numeric(N),
-                         strand = numeric(N),
-                         GOAid = character(N),
-                         GOAdescr = character(N),
-                         stringsAsFactors = FALSE)
-row.names(myGeneData) <- mySymbols
+# Add gene start, end, and strand information:
+# Initialize columns
+Chr20GeneData$start <- as.numeric(NA)
+Chr20GeneData$end <- as.numeric(NA)
+Chr20GeneData$strand <- as.numeric(NA)
 
-# Process everything into a dataframe
-for (sym in mySymbols) {
-  sel <- which(tmp$`HGNC symbol` == sym)
-  # choose the most specific (lowest frequency) GOA
-  GOAs <- tmp$`GOSlim GOA Accession(s)`[sel]
-  topGOA <- names(which(myGOslimsTable[GOAs] == min(myGOslimsTable[GOAs]))[1])
-  idx <- (sel[GOAs == topGOA])[1]
-
-  myGeneData[sym, "start"]    <- tmp$`Gene start (bp)`[idx]
-  myGeneData[sym, "end"]      <- tmp$`Gene end (bp)`[idx]
-  myGeneData[sym, "strand"]   <- tmp$Strand[idx]
-  myGeneData[sym, "GOAid"]    <- tmp$`GOSlim GOA Accession(s)`[idx]
-  myGeneData[sym, "GOAdescr"] <- tmp$`GOSlim GOA Description`[idx]
-
+# Add data
+for (i in 1:nrow(Chr20GeneData)) {
+  iRow <- which(tmp$`Gene stable ID` == Chr20GeneData$EnsemblID[i])
+  if (length(iRow) == 1) {
+    Chr20GeneData$start[i] <- tmp$`Gene start (bp)`[iRow]
+    Chr20GeneData$end[i] <- tmp$`Gene end (bp)`[iRow]
+    Chr20GeneData$strand[i] <- tmp$Strand[iRow]
+  }
 }
 
-write_tsv(myGeneData, path = "chr20_data.tsv")
 
 
 
-# =    4  STRING DATA  =========================================================
+# =    5  STRING DATA  =========================================================
 
 # Source data was downloaded from STRING database via organism specific
 # download.
@@ -132,16 +181,16 @@ write_tsv(myGeneData, path = "chr20_data.tsv")
 #
 
 # Read the alias data: we need that to find which ENSP IDs map to which
-# HUGO symbols
+# HGNC symbols
 tmp <- read_tsv(paste0(STRINGDIR, "9606.protein.aliases.v10.5.txt"),
                 skip = 1,
                 col_names = c("ENSP", "ID", "source"))  # 2,055,779 rows
 
 tmp <- tmp[grep("BioMart_HUGO", tmp$source), ]  # 19,119 rows
 
-tmp <- tmp[tmp$ID %in% myGeneData$sym, ] # 498 of 525 symbols mapped
-ENS2symMap <- tmp$ID                     # extract symbols...
-names(ENS2symMap) <- tmp$ENSP            # ... and use ENSP IDs as names
+tmp <- tmp[tmp$ID %in% Chr20GeneData$sym, ] # 497 of 521 symbols mapped
+ENS2symMap <- tmp$ID                        # extract symbols...
+names(ENS2symMap) <- tmp$ENSP               # ... and use ENSP IDs as names
 
 
 # Read the interaction graph data: this is a weighted graph defined as an
@@ -157,7 +206,7 @@ tmp <- tmp[tmp$score >= 900, ]  # 547,620 rows of high-confidence edges
 Chr20funcIntx <- tmp[(tmp$a %in% names(ENS2symMap)) &
                      (tmp$b %in% names(ENS2symMap)), ]  # 564 rows
 
-# Use ENS2symMap to translate ENSP IDs to HUGO symbols
+# Use ENS2symMap to translate ENSP IDs to HGNC symbols
 Chr20funcIntx$a <- ENS2symMap[Chr20funcIntx$a]
 Chr20funcIntx$b <- ENS2symMap[Chr20funcIntx$b]
 
